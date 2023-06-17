9e009672726fa3e3bf0428da4f08e4c27dc9d10f
TczDev <saranraj@muhabalab.com>
2023-06-17 15:31:29 +0530

    new

---
+++ assets/js/style.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('header');
+    if (window.scrollY > 100) {
+      header.classList.add('scrolled-header');
+    } else {
+      header.classList.remove('scrolled-header');
+    }
+  });
+
